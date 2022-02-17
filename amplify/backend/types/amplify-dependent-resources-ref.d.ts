6bdd958d34cdbf646c9e7d75542e3c3a85e43e51
Avinashparakhiya <avinashparakhiya@gmail.com>
2022-02-17 11:41:35 +0530

    after

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "17022021": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
